f700f60b98bde63fa331fd665ce70fb1abe69d47
qumaujean <qumaujea@student.42.fr>
2021-04-16 13:37:45 +0200

    Config typescript storybook/published module

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,5 +5,34 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
     'storybook-addon-material-ui'
-  ]
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      // propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
+  webpackFinal: (config) => {
+    config.resolve.extensions.push('.tsx');
+    config.resolve.extensions.push('.ts');
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-react',
+          '@babel/preset-typescript'
+        ]
+      }
+    });
+    return config;
+  }
 }
