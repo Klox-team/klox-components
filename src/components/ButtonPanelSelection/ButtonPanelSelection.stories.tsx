4703fdade29d2a09748097e8ce1c989146068acf
qumaujean <qumaujea@student.42.fr>
2021-05-04 12:32:23 +0200

    ButtonPanelSelection : Fix width height css

--- src/components/ButtonPanelSelection/ButtonPanelSelection.stories.tsx
+++ src/components/ButtonPanelSelection/ButtonPanelSelection.stories.tsx
@@ -20,6 +20,20 @@ export const Default = Template.bind({});
 export const WithLabel = Template.bind({});
 WithLabel.args = {
   label: "With label",
-  selection: ["With label selection", "With label 2 longer text button title"],
-  selected: ["With label 2 longer text button title"],
+  selection: ["With label selection", "With label 2"],
+  selected: ["With label 2"],
+};
+
+export const ShortText = Template.bind({});
+ShortText.args = {
+  label: "Short text",
+  selection: ["-18", "Tout", "18-25"],
+  selected: ["Tout", "18-25"],
+};
+
+export const LongText = Template.bind({});
+LongText.args = {
+  label: "Long text",
+  selection: ["THIS IS A LONG TEXT", "THIS IS AN EVEN LONGER LONG TEXT"],
+  selected: ["THIS IS AN EVEN LONGER LONG TEXT"],
 };
