a968046d89d8d6dbac6f0cf779b1430221dda851
qumaujean <qumaujea@student.42.fr>
2021-05-10 12:12:02 +0200

    Fix index import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { ButtonPanelSelection } from "./components/ButtonPanelSelection/ButtonPanelSelection";
-import { Header } from "./components/Header/Header";
 import { KloxButton } from "./components/KloxButton/KloxButton";
 import { KloxCard } from "./components/KloxCard/KloxCard";
 import { KloxCheckBox } from "./components/KloxCheckBox/KloxCheckBox";
@@ -9,7 +8,6 @@ import { StripeTextField } from "./components/StripeTextField/StripeTextField";
 
 export {
   ButtonPanelSelection,
-  Header,
   KloxButton,
   KloxCard,
   KloxCheckBox,
